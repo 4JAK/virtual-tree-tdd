eca93f952d33eff7447c2d6bf4fe2852cbcd6196
julian-perge <julianperge@gmail.com>
2018-07-30 11:29:00 -0400

    Added initial Virtual Tree testing

---
+++ src/test/java/org/wecancodeit/virtualtreetdd/VirtualTreeTest.java
@@ -0,0 +1,43 @@
+package org.wecancodeit.virtualtreetdd;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class VirtualTreeTest {
+
+  @Mock VirtualTree testTree;
+  
+  @Before
+  public void treeSetup() {
+	  testTree = new VirtualTree("Java Tree");
+  }
+
+  @Test
+  public void shouldBeAbleToCreateNewVirtualTree() {
+    assertNotNull(testTree);
+  }
+
+  @Test
+  public void shouldHaveName() {
+    assertThat(testTree.getName(), is(equalTo("Java Tree")));
+  }
+
+  @Test
+  public void shouldHaveGrowthUnit() {
+	  assertNotNull(testTree.getGrowth());
+  }
+  
+  @Test
+  public void wateringTreeIncreasesGrowth() {
+	  testTree.water();
+	  assertThat(testTree.getGrowth(), is(greaterThan(0)));
+  } 
+}
