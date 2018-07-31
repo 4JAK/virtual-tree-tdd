b0f69e72764bbba65f1057800ed12e5841f79d0c
Jeanne Clair <jeanne.m.clair@gmail.com>
2018-07-31 12:48:44 -0400

    Added more in-depth testing of entities

--- src/test/java/org/wecancodeit/virtualtreetdd/BranchTest.java
+++ src/test/java/org/wecancodeit/virtualtreetdd/BranchTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,13 +12,11 @@ import org.mockito.Mock;
 public class BranchTest {
 
 	@Mock
-	Branch testBranch;
+	private Branch testBranch;
 
 	@Before
 	public void branchSetup() {
-
 		testBranch = new Branch("Java Branch", new VirtualTree());
-
 	}
 
 	@Test
@@ -31,9 +28,10 @@ public class BranchTest {
 	public void shouldHaveName() {
 		assertThat(testBranch.getName(), is(equalTo("Java Branch")));
 	}
-	
+
 	@Test
-	public void shouldHaveVirtualTree () {
+	public void shouldHaveVirtualTree() {
 		assertNotNull(testBranch.getVirtualTree());
 	}
+
 }
