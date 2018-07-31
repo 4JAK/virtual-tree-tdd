704d8d3314d37f7eb8de47e5d97150a1237b2a42
julian-perge <julianperge@gmail.com>
2018-07-31 14:57:38 -0400

    Modified Cluster class to include collection of beans. Added Bean class

---
+++ src/main/java/org/wecancodeit/virtualtreetdd/Bean.java
@@ -0,0 +1,60 @@
+package org.wecancodeit.virtualtreetdd;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Bean {
+  @Id @GeneratedValue private Long id;
+
+  @Lob private String example;
+  private String questionNum;
+  private String question;
+  private String answer;
+
+  @ManyToOne private Cluster cluster;
+
+  /**
+   * @param example - The example given before the question that is going to be asked
+   * @param questionNum
+   * @param question - The question that will be shown
+   * @param answer
+   * @param cluster - Cluster of which the bean is owned by
+   */
+  public Bean(String example, String questionNum, String question, String answer, Cluster cluster) {
+    this.example = example;
+    this.questionNum = questionNum;
+    this.question = question;
+    this.answer = answer;
+    this.cluster = cluster;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getExample() {
+    return example;
+  }
+
+  public String getQuestionNum() {
+    return questionNum;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public String getAnswer() {
+    return answer;
+  }
+
+  public Cluster getCluster() {
+    return cluster;
+  }
+
+  protected Bean() {}
+}
--- src/main/java/org/wecancodeit/virtualtreetdd/Cluster.java
+++ src/main/java/org/wecancodeit/virtualtreetdd/Cluster.java
@@ -1,42 +1,44 @@
 package org.wecancodeit.virtualtreetdd;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Cluster {
 
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String name;
-	
-	@ManyToOne
-	private Branch branch;
+  @Id @GeneratedValue private Long id;
+  private String name;
 
-	public Cluster(String name, Branch branch) {
-		this.name = name;
-		this.branch = branch;
-	} 
-	
-	public Cluster() {
-		
-	}
+  @ManyToOne private Branch branch;
 
-	public Long getId() {
-		return id;
-	}
+  @OneToMany(mappedBy = "cluster")
+  private Collection<Bean> beans;
 
-	public String getName() {
-		return name;
-	}
+  public Cluster(String name, Branch branch) {
+    this.name = name;
+    this.branch = branch;
+  }
 
-	public Branch getBranch() {
-		return branch;
-	}
+  public Cluster() {}
 
-	
+  public Collection<Bean> getBeans() {
+    return beans;
+  }
 
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Branch getBranch() {
+    return branch;
+  }
 }
