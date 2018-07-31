83528a7b9dde913e6eb33ee5f7475b650709e4bb
Jeanne Clair <jeanne.m.clair@gmail.com>
2018-07-31 12:43:55 -0400

    Added @Repository testing

--- src/main/java/org/wecancodeit/virtualtreetdd/ClusterRepository.java
+++ src/main/java/org/wecancodeit/virtualtreetdd/ClusterRepository.java
@@ -1,7 +1,9 @@
 package org.wecancodeit.virtualtreetdd;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClusterRepository extends CrudRepository<Cluster, Long> {
 
 }
