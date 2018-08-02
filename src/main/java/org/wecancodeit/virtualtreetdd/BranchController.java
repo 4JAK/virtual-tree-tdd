3ef0bcd26d70f4dd1bad2fe4255d55d4479ec45d
julian-perge <julianperge@gmail.com>
2018-08-01 23:35:36 -0400

    Modified mapping method signatures to be more descriptive. Changed single branch mapping attribute value to point to the branch repo. Tests of controller modified accordingly.

--- src/main/java/org/wecancodeit/virtualtreetdd/BranchController.java
+++ src/main/java/org/wecancodeit/virtualtreetdd/BranchController.java
@@ -3,29 +3,23 @@ package org.wecancodeit.virtualtreetdd;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BranchController {
 
-	@Autowired
-	private BranchRepository branchRepo;
-	
-	@Autowired
-	private VirtualTreeRepository vTreeRepo;
-	
-	
-	
-	@RequestMapping (value = "branch")
-	public String branch(Model model) {
-		model.addAttribute("branch", branchRepo.findAll());
-		return "branch";
-	}
+  @Autowired private BranchRepository branchRepo;
 
-	@RequestMapping (value = "branches")
-	public String branches(Model model) {
-		model.addAttribute("branches", vTreeRepo.findOne(1L).getBranches());
-		return "branches";
-	}
+  @RequestMapping(value = "/branches")
+  public String getBranches(Model model) {
+    model.addAttribute("branches", branchRepo.findAll());
+    return "branches";
+  }
 
+  @RequestMapping(value = "/branch/{id}")
+  public String getBranch(@PathVariable(name = "id") Long id, Model model) {
+    model.addAttribute("branch", branchRepo.findOne(id));
+    return "branch";
+  }
 }
--- src/test/java/org/wecancodeit/virtualtreetdd/BranchControllerTest.java
+++ src/test/java/org/wecancodeit/virtualtreetdd/BranchControllerTest.java
@@ -1,7 +1,6 @@
 package org.wecancodeit.virtualtreetdd;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,58 +18,51 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest(BranchController.class)
 public class BranchControllerTest {
+  @Autowired MockMvc mvc;
+  
+  @MockBean private VirtualTreeRepository vTreeRepo;
+  @MockBean private BranchRepository branchRepo;
 
-	@Autowired
-	MockMvc mvc;
+  @Mock VirtualTree virtualTree;
 
-	@MockBean
-	private VirtualTreeRepository vTreeRepo;
-	
-	@MockBean
-	private BranchRepository branchRepo;
-	
-	@Mock
-	VirtualTree virtualTree;
+  @Test
+  public void branchMappingShouldReturn2xxSuccessful() throws Exception {
+    mvc.perform(get("/branch/1")).andExpect(status().isOk()).andExpect(status().is2xxSuccessful());
+  }
 
-	@Test
-	public void branchMappingShouldReturn2xxSuccessful() throws Exception {
-		mvc.perform(get("/branch")).andExpect(status().isOk()).andExpect(status().is2xxSuccessful());
-	}
+  @Test
+  public void branchMappingShouldReturn4xxError() throws Exception {
+    mvc.perform(get("/brances/1")).andExpect(status().is4xxClientError());
+  }
 
-	@Test
-	public void branchMappingShouldReturn4xxError() throws Exception {
-		mvc.perform(get("/brances")).andExpect(status().is4xxClientError());
-	}
+  @Test
+  public void branchMappingAttributeShouldHaveHaveBranch() throws Exception {
+    mvc.perform(get("/branch/1")).andExpect(model().attribute("branch", is(branchRepo.findAll())));
+  }
 
-	@Test
-	public void branchMappingAttributeShouldHaveHaveBranch() throws Exception {
-		mvc.perform(get("/branch")).andExpect(model().attribute("branch", is(branchRepo.findAll())));
-	}
+  @Test
+  public void branchMappingViewNameShouldBeBranch() throws Exception {
+    mvc.perform(get("/branch/1")).andExpect(view().name(is("branch")));
+  }
 
-	@Test
-	public void branchMappingViewNameShouldBeBranch() throws Exception {
-		mvc.perform(get("/branch")).andExpect(view().name(is("branch")));
-	}
-	
-	@Test
-	public void branchesMappingShouldReturn2xxSuccessful() throws Exception {
-		mvc.perform(get("/branches")).andExpect(status().isOk()).andExpect(status().is2xxSuccessful());
-	}
+  @Test
+  public void branchesMappingShouldReturn2xxSuccessful() throws Exception {
+    mvc.perform(get("/branches")).andExpect(status().isOk()).andExpect(status().is2xxSuccessful());
+  }
 
-	@Test
-	public void branchesMappingShouldReturn4xxError() throws Exception {
-		mvc.perform(get("/brances")).andExpect(status().is4xxClientError());
-	}
+  @Test
+  public void branchesMappingShouldReturn4xxError() throws Exception {
+    mvc.perform(get("/brances")).andExpect(status().is4xxClientError());
+  }
 
-	@Test
-	public void branchesMappingAttributeShouldHaveHaveBranches() throws Exception {
-		given(vTreeRepo.findOne(1L)).willReturn(virtualTree);
-		mvc.perform(get("/branches")).andExpect(model().attribute("branches", is(vTreeRepo.findOne(1L).getBranches())));
-	}
+  @Test
+  public void branchesMappingAttributeShouldHaveHaveBranches() throws Exception {
+    mvc.perform(get("/branches"))
+        .andExpect(model().attribute("branches", is(branchRepo.findAll())));
+  }
 
-	@Test
-	public void branchesMappingViewNameShouldBeBranches() throws Exception {
-		mvc.perform(get("/branches")).andExpect(view().name(is("branches")));
-	}
-
+  @Test
+  public void branchesMappingViewNameShouldBeBranches() throws Exception {
+    mvc.perform(get("/branches")).andExpect(view().name(is("branches")));
+  }
 }
