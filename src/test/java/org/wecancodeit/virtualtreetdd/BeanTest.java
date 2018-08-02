1d01a755b5bd3632e50e58bfe88a621ea6b9b6ff
julian-perge <julianperge@gmail.com>
2018-08-02 00:59:53 -0400

    Added @Before, allowing less code for the tests.

--- src/test/java/org/wecancodeit/virtualtreetdd/BeanTest.java
+++ src/test/java/org/wecancodeit/virtualtreetdd/BeanTest.java
@@ -5,35 +5,44 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.wecancodeit.virtualtreetdd.Bean.QuestionType;
 
 public class BeanTest {
 
+  private Bean testBean;
+
+  @Before
+  public void setUp() {
+    Cluster testCluster = new Cluster();
+    testBean = new Bean(QuestionType.TrueOrFalse, "What is 42?", "The answer to life", testCluster);
+  }
+
   @Test
   public void shouldBeAbleToInstaniate() {
-    Bean testBean = new Bean();
-    assertNotNull(testBean);
+    Bean underTestBean = new Bean();
+    assertNotNull(underTestBean);
   }
 
   @Test
   public void beanShouldHaveSingleQuestion() {
-    Bean testBean = new Bean(null, "Java Bean Example", "1", "a single question?", null, null);
-
     String underTestBeanQuestion = testBean.getQuestion();
     assertNotNull(underTestBeanQuestion);
   }
 
   @Test
   public void beanQuestionTypeShouldBeTrueOrFalse() {
-	  Bean testBean = new Bean(QuestionType.TrueOrFalse, null, null, null, "1", null);
-	  
-	  assertThat(testBean.getQuestionType(), is(QuestionType.TrueOrFalse));
+    assertThat(testBean.getQuestionType(), is(QuestionType.TrueOrFalse));
   }
-  
-  @Test public void beanQuestionTypeShouldNotBeTrueOrFalse() {
-	  Bean testBean = new Bean(QuestionType.TrueOrFalse, null, null, null, "1", null);
-	  
-	  assertThat(testBean.getQuestionType(), is(not(QuestionType.FillInTheBlanks)));
+
+  @Test
+  public void beanQuestionTypeShouldNotBeTrueOrFalse() {
+    assertThat(testBean.getQuestionType(), is(not(QuestionType.FillInTheBlanks)));
+  }
+
+  @Test
+  public void beanShouldHaveCluster() {
+    assertNotNull(testBean.getCluster());
   }
 }
