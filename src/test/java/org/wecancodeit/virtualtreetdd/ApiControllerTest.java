38d958cb63e66f1991a2c38456f8d3d5d2f6f8a1
Jeanne Clair <jeanne.m.clair@gmail.com>
2018-08-06 15:28:42 -0400

    updated api controller for next bean. need to finish

--- src/test/java/org/wecancodeit/virtualtreetdd/ApiControllerTest.java
+++ src/test/java/org/wecancodeit/virtualtreetdd/ApiControllerTest.java
@@ -18,108 +18,143 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApiControllerTest {
-	
+
 	@Resource
 	TestRestTemplate restTemplate;
-	
+
 	@Test
 	public void canary() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.OK));
 	}
-	
+
 	@Test
 	public void shouldBeOkForVirtualTrees() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees", String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.OK));
 	}
-	
-	@Test public void shouldNotBeOkayForVirtualTrees() {
+
+	@Test
+	public void shouldNotBeOkayForVirtualTrees() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/2", String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.NOT_FOUND));
 	}
-	
+
 	@Test
 	public void shouldBeOkForBranches() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches", String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.OK));
 	}
-	
-	@Test public void shouldNotBeOkayForBranches() {
+
+	@Test
+	public void shouldNotBeOkayForBranches() {
 		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/brances", String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.NOT_FOUND));
 	}
-	
+
 	@Test
 	public void shouldBeOkForClusters() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters", String.class);
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters",
+				String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.OK));
 	}
-	
-	@Test public void shouldNotBeOkayForClusters() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clustes", String.class);
+
+	@Test
+	public void shouldNotBeOkayForClusters() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clustes",
+				String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.NOT_FOUND));
 	}
-	
+
 	@Test
 	public void shouldBeOkForBeans() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters/1/beans", String.class);
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters/1/beans",
+				String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.OK));
 	}
-	
-	@Test public void shouldNotBeOkayForBeans() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters/1/bans", String.class);
+
+	@Test
+	public void shouldNotBeOkayForBeans() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters/1/bans",
+				String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.NOT_FOUND));
 	}
-	
+
 	@Test
 	public void shouldBeOkForSingleBean() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters/1/beans/1", String.class);
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters/1/beans/1",
+				String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.OK));
 	}
-	
-	@Test public void shouldNotBeOkayForSingleBean() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters/1/bans/1", String.class);
+
+	@Test
+	public void shouldNotBeOkayForSingleBean() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/virtualtrees/1/branches/1/clusters/1/bans/1",
+				String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.NOT_FOUND));
 	}
-	
+
 	@Test
 	public void shouldBeOkForCheckAnswerOfBean() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/beans/1/checkanswer?answerToCheck=test1", String.class);
-		HttpStatus status = response.getStatusCode();		
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/beans/1/checkanswer?answerToCheck=test1",
+				String.class);
+		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.OK));
 	}
-	
-	@Test 
+
+	@Test
 	public void shouldNotBeOkayForCheckAnswerOfBean() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/beeans/1/checkanswer?answerToCheck=test1", String.class);
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/beeans/1/checkanswer?answerToCheck=test1",
+				String.class);
 		HttpStatus status = response.getStatusCode();
 		assertThat(status, is(HttpStatus.NOT_FOUND));
 	}
-	
+
 	@Test
 	public void shouldReturnFalseForCorrectAnswerToBean() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/beans/1/checkanswer?answerToCheck=test1", String.class);
-		String body = response.getBody();		
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/beans/1/checkanswer?answerToCheck=test1",
+				String.class);
+		String body = response.getBody();
 		assertThat(body, is("false"));
-		
 	}
-	
+
 	@Test
 	public void shouldReturnTrueForCorrectAnswerToBean() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/beans/5/checkanswer?answerToCheck=true", String.class);
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/beans/5/checkanswer?answerToCheck=true",
+				String.class);
 		String body = response.getBody();
 		assertThat(body, is("true"));
 	}
+	
+	@Test
+	public void shouldBeOkForNextBeanFromClusterCollection() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/clusters/1/bean", String.class);
+		HttpStatus status = response.getStatusCode();
+		assertThat(status, is(HttpStatus.OK));
+	}
+
+	@Test
+	public void shouldNotBeOkayForNextBeanFromClusterCollection() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/clusters/1/bean", String.class);
+		HttpStatus status = response.getStatusCode();
+		assertThat(status, is(HttpStatus.NOT_FOUND));
+	}
+	
+//	@Test
+//	public void shouldGetNextBeanFromClusterCollection() {
+//		ResponseEntity<String> response = restTemplate.getForEntity("/api/clusters/1/beans/", String.class);
+//		
+//		
+//	}
 }
