9ca35ce7552aa5c3fa1aa11bed6b6e16ced86d64
julian-perge <julianperge@gmail.com>
2018-08-21 19:19:44 -0400

    Removed my beautiful face images and added current js

--- src/main/resources/static/js/beanIsLastInCluster.js
+++ src/main/resources/static/js/beanIsLastInCluster.js
@@ -1,7 +1,6 @@
 function goToNextCluster(response) {
   if (this.status === 200 && this.readyState === 4) {
     const cluster = JSON.parse(response.target.response);
-    console.log(cluster.branch);
     window.location.replace(`/cluster/${cluster.id}`);
   }
 }
@@ -14,18 +13,31 @@ function getNextCluster() {
   xhr.send();
 }
 
+function clusterIsLastInBranch(response) {
+  if (this.status === 200 && this.readyState === 4) {
+    const res = JSON.parse(response.target.response);
+    if (res === true) {
+      window.location.replace('/JavaTreeCompleted');
+    }
+  }
+}
+
 function beanIsLastInCluster(response) {
   if (this.status === 200 && this.readyState === 4) {
     const res = JSON.parse(response.target.response);
     if (res === true) {
+      const xhr = new XMLHttpRequest();
+      const hrefArray = document.URL.split('/');
+      const clusterId = hrefArray[4];
+      xhr.addEventListener('readystatechange', clusterIsLastInBranch);
+      xhr.open('GET', `/api/clusters/${clusterId}/checkIfLastClusterOnTree?clusterId=${clusterId}`, true);
+      xhr.send();
       document.getElementById('nextQuestionButton').innerText = 'Next Cluster';
       document.getElementById('nextQuestionButton').removeEventListener('click', getNextBeanQuestion);
       document.getElementById('nextQuestionButton').addEventListener('click', getNextCluster);
-      console.log('is last!');
-      // return true;
+    } else {
+      // console.log('Is not last in cluster');
     }
-    console.log('Is not last in cluster');
-    // return false;
   }
 }
 
@@ -33,8 +45,6 @@ function checkIfBeanIsLastInCluster() {
   const xhr = new XMLHttpRequest();
   const clusterId = document.querySelector('.clusterBeans').id;
   const beanId = document.querySelector('.bean-label').id;
-  console.log(`clusterid: ${clusterId} | beanId: ${beanId}`);
-
   xhr.addEventListener('readystatechange', beanIsLastInCluster);
   xhr.open('GET', `/api/clusters/${clusterId}/checkBean?beanId=${beanId}`, true);
   xhr.send();
